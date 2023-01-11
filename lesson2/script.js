ebe4abf9250a8ef59d9c4e53b510246a243b615c
“EGORBRUH” <evasilenko.ev@gmail.com>
2023-01-11 13:41:51 +0300

    first commit

--- lesson2/script.js
+++ lesson2/script.js
@@ -1,22 +1,18 @@
 'use strict'
 {
 
-  prompt('Денди');
-  prompt('Едениц товара');
-  prompt('Категория товара')
-  prompt('Цена товара')
 
-  const product = '"Приставка Денди"';
-  const number = 5;
-  const category = 'gameConsole';
-  const price = 200;
+
+  const product = '"Приставка денди"' + prompt('"Приставка Денди"');
+  const number = 5 + prompt('Едениц товара');
+  const category = 'Категория товара' + prompt('Категория товара');
+  const price = 200 + prompt('деревянных');
+
+
 
   console.log(typeof product);
-
   console.log(typeof number);
-
   console.log(typeof category);
-
   console.log(typeof price);
 
   console.log('На складе', number, 'едениц товара', product, 'на сумму', number * price, 'деревянных');
