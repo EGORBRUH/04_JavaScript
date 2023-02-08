7eca0d68fc0c4ff0203b40ab647b0ba768222f9d
“EGORBRUH” <evasilenko.ev@gmail.com>
2023-02-08 22:46:26 +0300

    lesson11

--- lesson11/js/task02.js
+++ lesson11/js/task02.js
@@ -1,13 +1,13 @@
 'use strict';
 
 
-const recFoo = (array) => {
+const task02 = (array) => {
   const arrNum = Math.floor(Math.random() * 11);
   array.push(arrNum);
     console.log(arrNum);
   if ((array.reduce((acc, curr) => acc + curr)) > 50) return array;
 
-  return recFoo(array);
+  return task02(array);
 }
 
-console.log(recFoo([1,2,3,4,5]));
+console.log(task02([1,2,3,4,5]));
