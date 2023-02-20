5c2f9d76bb3e18b92297b6975dc618988a488610
“EGORBRUH” <evasilenko.ev@gmail.com>
2023-02-20 12:38:33 +0300

    правки по итоговой

--- final_marbles/js/game.js
+++ final_marbles/js/game.js
@@ -10,7 +10,6 @@
     }
     return randomNum;
   };
-
   const gameObject = {
     ballPlayer: 5,
     ballBot: 5,
@@ -29,12 +28,12 @@
 
     console.log(result);
     return function play() {
-      const userInput = +prompt(`Сколько шариков из ${result.ballPlayer} вы хотите разыграть?`);
+      const userInput = prompt(`Сколько шариков из ${result.ballPlayer} вы хотите разыграть?`);
       const playObject = gameObject;
-      if (Number.isNaN(userInput)) {
+      if (Number.isNaN(+userInput) || userInput == 0) {
        alert('Введите число!!!');
        return play();
-      }if (userInput === null) {
+      } if (userInput === null || 0) {
         console.log(userInput);
         const question = confirm('Уже уходите?');
         if (question === true) {
@@ -43,48 +42,38 @@
         } else {
           return play();
         }
-      } if (userInput > 10 || userInput < 1) {
+      } if (userInput > result.ballPlayer && userInput != result.ballPlayer) {  // Как запретить вводить строку?
+        console.log(typeof userInput)
         alert('Введите корректное значение!');
         return play();
       }
-
       const computerPlayer = randomBot();
       console.log(userInput);
-
-
       console.log(computerPlayer);
-      if (userInput % 2 === 0 && computerPlayer % 2 === 0 ||
-        userInput % 2 === 1 && computerPlayer % 2 === 1) {
-        alert(`Игрок выбрал: ${userInput} \n Компьютер выбрал ${computerPlayer} \n Ничья!`);
-        const more = confirm('Сыграем еще?');
-        if (more === true) {
-          return play();
-        } else {
-          playObject.finalScore;
-          return;
-        }
-      }
+
       if (userInput % 2 === 0 && computerPlayer === 2 ||
         userInput % 2 === 0 && computerPlayer === 1) {
-        alert('Вы не угадали!');
+        alert('Бот угадал! Вы проиграли!');
         result.ballPlayer -= +userInput;
         result.ballBot += +userInput;
-      }  else {
-        alert('Вы угадали!');
+      }
+      else {
+        alert('Вы выйграли ');
         result.ballBot -= +userInput;
         result.ballPlayer += +userInput;
-        if (result.ballPlayer <= 0 === 0 || result.ballPlayer >= 10 === 10 &&
-          result.ballBot <= 0 === 0 || result.ballBot >= 10 === 10) {
-        }
       }
-      if (result.ballPlayer <= 0 || result.ballBot <= 0 ||
+      if (result.ballPlayer <= 0 === 0 || result.ballPlayer >= 10 === 10 &&
+        result.ballBot <= 0 === 0 || result.ballBot >= 10 === 10) {
+
+      } if (result.ballPlayer <= 0 || result.ballBot <= 0 ||
         result.ballPlayer >= 10 || result.ballBot >= 10) {
         alert('Игра окончена!');
         result.finalScore;
-        return;
+        return
       }
       return play();
     };
+
   };
 
   window.RPS = game;
